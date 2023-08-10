10d07c62a4a05c953d5234939af3975bd9046d85
ayin86cn <ayin86cn@gmail.com>
2023-08-10 20:41:04 +0800

    readme

--- src/views/sub/layoutCom.tsx
+++ src/views/sub/layoutCom.tsx
@@ -1,22 +1,8 @@
 import {Outlet} from "react-router-dom"
-//lazy load error don't know why!
-// const HeaderInnner=lazy(()=>import("./header/index-header"));
-// const SiderInner=lazy(()=>import("./sider/index-sider"));
-
 export default () => {
   return (
     <div className="layout-common">
-      123
       <Outlet/>
     </div>
   );
 };
-// function Login(){
-//   return (
-//     <div>
-//       <div>layout</div>
-//       <Outlet/>
-//     </div>
-//   )
-// }
-// export default Login
