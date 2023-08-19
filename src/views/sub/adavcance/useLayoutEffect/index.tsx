117e8c57e936de65cbaec576cea0cab295f26d83
ayin86cn <ayin86cn@gmail.com>
2023-08-19 19:02:46 +0800

    20230819

--- src/views/sub/adavcance/useLayoutEffect/index.tsx
+++ src/views/sub/adavcance/useLayoutEffect/index.tsx
@@ -1,5 +1,24 @@
+import {useState,useEffect,useLayoutEffect} from "react"
 export default ()=>{
+  const [count,setCount]=useState(0)
+  const changeCount=()=>{
+    setCount(0)
+  }
+  //闪动
+  // useEffect(()=>{
+  useLayoutEffect(()=>{
+    if(count===0){
+      setCount(20+Math.random()*100)
+    }
+  },[count])
   return (
-    <div>React高阶知识--useLayoutEffect</div>
+    <div>React高阶知识--useLayoutEffect
+      <p>useEffect是在渲染时异步执行，并且等到浏览器将变化渲染到屏幕后才会执行</p>
+      <p>在这个案例中，被渲染了两次</p>
+      <p>useLayoutEffect时在dom结构更新后、渲染前执行，在渲染时时同步执行，具有防抖的效果。</p>
+      {count}
+      <hr />
+      <button onClick={changeCount}>setCount</button>
+    </div>
   )
 }
